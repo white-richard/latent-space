73253231e375ac8a431502f151af3c1003a11939
white-richard <rich.white285@yahoo.com>
2026-03-19 15:07:33 -0700

    finish setup script

--- setup.fish
+++ setup.fish
@@ -1,7 +1,24 @@
 #!/usr/bin/env fish
 
-uv pip install -r requirements.txt
-uv sync --extra dev
-# check for --dino boolean flag, if so, then
-uv pip install -e $PWD/repo/dinov3
-uv sync --extra dinov3
+set -e VIRTUAL_ENV
+
+set latent_dir "$HOME/.code/latent-space/"
+
+set project_dir $PWD
+
+# Parse --dino flag
+set use_dino false
+for arg in $argv
+    if test "$arg" = "--dino"
+        set use_dino true
+    end
+end
+
+uv pip install -r $latent_dir/requirements.txt
+uv sync --extra dev --project $latent_dir
+uv pip install -e $latent_dir
+
+if test "$use_dino" = true
+    uv pip install -e $latent_dir/repos/dinov3
+    uv sync --extra dinov3 --project $latent_dir
+end
