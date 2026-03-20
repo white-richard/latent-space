b9dd1f27e9eca09e9546e78f3329064ed640e58a
white-richard <rich.white285@yahoo.com>
2026-03-19 21:16:05 -0700

    fix uv sync bug in setup

--- setup.fish
+++ setup.fish
@@ -14,11 +14,12 @@ for arg in $argv
     end
 end
 
-uv pip install -r $latent_dir/requirements.txt
-uv sync --extra dev --project $latent_dir
-uv pip install -e $latent_dir
-
 if test "$use_dino" = true
-    uv pip install -e $latent_dir/repos/dinov3
-    uv sync --extra dinov3 --project $latent_dir
+    uv sync --extra dev --extra dinov3 --project $latent_dir
+else
+    uv sync --extra dev --project $latent_dir
 end
+
+uv pip install -r $latent_dir/requirements.txt
+uv pip install -e $latent_dir
+test "$use_dino" = true && uv pip install -e $latent_dir/repos/dinov3
