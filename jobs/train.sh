7a29ca5f2ffacfac07c0a22f011703c4e2344be0
white-richard <rich.white285@yahoo.com>
2026-03-25 01:15:12 -0700

    update train [skip ci]

--- jobs/train.sh
+++ jobs/train.sh
@@ -16,6 +16,7 @@ docker run --rm --gpus all \
   -e SLURM_JOB_ID="$SLURM_JOB_ID" \
   -v /home/slurm-jobs/.ssh/github_deploy:/root/.ssh/github_deploy:ro \
   -v /home/slurm-jobs/.ssh/dvc_key:/root/.ssh/dvc_key:ro \
+  -v /home/richw/.code/latent-space:/root/.code/latent-space:ro \
   -v "${HOME}/.cache/uv:/root/.cache/uv" \
   ml-runner:latest bash -c '
     chmod 600 /root/.ssh/github_deploy /root/.ssh/dvc_key
@@ -25,14 +26,12 @@ docker run --rm --gpus all \
     git submodule update --init --recursive
     fish setup.fish --dino
     source .venv/bin/activate
-    dvc remote add -d --local wpeb-print ssh://wpeb-print/home/richw/.code/latent-space/.dvc/cache
-    dvc remote modify --local wpeb-print keyfile /root/.ssh/dvc_key
-    ssh-keyscan wpeb-print >> ~/.ssh/known_hosts 2>/dev/null
-    dvc pull datasets/cifar.dvc
+    dvc remote add -d --local local-cache /root/.code/latent-space/.dvc/cache
+    dvc pull /root/.code/latent-space/datasets/cifar.dvc
     nvidia-smi
     fish src/cifar_lightning/run.fish --debug-mode
-    git config user.email "slurm@local"
-    git config user.name "Richard White"
+    git config user.email "98299003+white-richard@users.noreply.github.com"
+    git config user.name "white-richard"
     git add out/ 2>/dev/null || true
     git diff --cached --quiet || git push origin HEAD
   '
