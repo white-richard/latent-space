f1d7cf7c7d43c3d6f8d23dc1c0c82753910f199c
white-richard <98299003+white-richard@users.noreply.github.com>
2026-05-13 21:37:25 -0700

    update ip to desk

--- scripts/start_mlflow.fish
+++ scripts/start_mlflow.fish
@@ -5,5 +5,5 @@ mlflow server \
     --host 0.0.0.0 \
     --port 5050 \
     --serve-artifacts \
-    --allowed-hosts "172.20.199.236,172.20.199.236:5050,100.121.43.41,100.121.43.41:5050,localhost,localhost:5050" \
-    --cors-allowed-origins "http://100.121.43.41:5050,http://localhost:5050,http://172.20.199.236:5050"
+    --allowed-hosts "172.20.199.236,172.20.199.236:5050,100.100.16.25,100.100.16.25:5050,localhost,localhost:5050" \
+    --cors-allowed-origins "http://100.100.16.25:5050,http://localhost:5050,http://172.20.199.236:5050"
